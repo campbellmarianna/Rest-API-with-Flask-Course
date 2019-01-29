3f3c64d2c43fa3b4f2dc088fcd36a9a9c8764bc0
Marianna <mckd2018@gmail.com>
2019-01-29 11:47:03 -0800

    add first challenge

---
+++ lists_tuples_sets.py
@@ -0,0 +1,11 @@
+my_variable = "hello"
+
+grades = [77, 80, 90, 95, 100, 105, 107]
+
+tuple_grades = (77, 80, 90, 95, 100, 105, 107, 108, 109) # immutable(cannot increase size, cannot be changed)
+
+set_grades = {77, 80, 90, 95, 100, 105, 107, 108, 109} # unique & unordered
+
+set_grades.add(60)
+set_grades.add(60)
+print(set_grades)
