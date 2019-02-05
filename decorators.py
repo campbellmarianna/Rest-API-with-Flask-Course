b9a364bde70cc31be03285591299b475baeff8dd
Marianna <mckd2018@gmail.com>
2019-02-05 14:39:05 -0800

    add implementation of advanced decorators

--- decorators.py
+++ decorators.py
@@ -13,4 +13,26 @@ def my_decorator(func): # the decorator should always call the function
 def my_function(): # changes the function
     print("I'm the function!")
 
-my_function()
+# my_function()
+
+##
+# This is something we can do in python - develop functions inside functions, 3 levels of defining functions it doesn't go any deeper
+def decorator_with_arguments(number):
+    def my_decorator(func): # this is the decorator that creates the wrap function - that tells us that the function below is going to replace the my_function_too function
+        @functools.wraps(func)
+        def function_that_runs_func(*args, **kwargs): # your decorators should alaways have parameters
+            print("In the decorator!")
+            if number == 56: # example if the function is the admin page, if the user doesn't have the appropiate permission we don't show the admin page
+                print("Not running the function!")
+            else:
+                func()
+            print("After the decorator!")
+        return function_that_runs_func
+    return my_decorator
+
+# a more complex decorator can except arguments itself
+@decorator_with_arguments(56) # example: insert elements into an database
+def my_function_too(x, y):
+    print(x + y )
+
+my_function_too(57, 67)
