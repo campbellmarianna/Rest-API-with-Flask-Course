a2d34ed16b67f773d36f3ace9cdaecc6b6f196fc
Marianna <mckd2018@gmail.com>
2019-02-10 19:43:11 -0800

    add REST principals

--- flask-app/app.py
+++ flask-app/app.py
@@ -14,45 +14,38 @@ def home(): # create a method home
 app.run(port=5000)# we have to tell the app to start running
 # we have to tell the a specific port an area of the computer where your app is going to receiving your request and returning your response through
 
-# HTTP is one of the most popular protocal to create interations and allowing interactions between to internet connected elements
-# What is a web server?
-    # hardware
-    # software designed to accept incoming web requests
-    # For example, Google has many web servers
-    # When we go to http://www.google.com in our browser, we send something to a web server
-        # Then it can decide to respond
-        # Left off at 38. HTTP Verbs 01:52
-# What do we send?
-    # WHen you go to http://www.google.com, you send the following:
-        # GET / HTTP/1.1
-        # Host: www.google.com
-        # What does this mean:
-            # It is a GET request
-                # The server then sees GET (a verb - tells the server to some extent what the server will return, it is what we expect of the server) / (path - what we want out of the server) HTTP (protocol - the most popular protocal)/1.1
-# So, what?
-    # That's it!
-    # The server sees that, and then there's code
-    # The code may interpret the GET request in many different ways
-# For example ...
-    # It may give you an error, if / is not found
-    # It may give you an error, if HTTP is not supported
-    # It may give you an error, if the server is unavailable
-    # It may give you HTML code back (which is what it normally does)
-    # It may give you some text back
-    # It may give you nothing back
-# What else?
-    # Going to any page in your browser will do the same
-# Differences
-# THe only difference is what the server on the other end responds with
-    # Twitter responds with the Twitter HTML
-    # git-scm responds with the git-scm HTML
-    # Google responds witht the Google HTML
-# What else?
-    # Going to a page will always do a GET # the browser (Chrome, Safari) is configured to do a GET request
-    # But there are many other things we can do, such as POST, DELETE, PUT, OPTIONS, HEAD and much more
-    # Each server responds differently to each, but they normally have the same meaning in each
-# HTTP Verbs
-    # GET Retrieve something EX: GET /item/1
-    # POST Receive data, and use it EX: POST/item # with a post request normally, you have to send some data along the post request
-    # PUT Make sure something is there EX: PUT/item # check if there if something is there it will update it
-    # DELETE Remove somehting EX: DELETE /item/1
+
+# What we know
+    # Going to a site does a GET request
+    # This normally returns HTML
+    # We can return other things
+    # We can do other than GET EX: POST
+# What is a REST API?
+# It's a way of thinking about how a web server responds to your requests
+# It doesn't respond with just data
+# It responds with resources (the server ask for something and responses with data this is different the server is asking for a resource a thing in the server)
+# Resources?
+# Similar to object-oriented programming
+# Think of the server as having resources, and each is able to interact with the pertinent request
+
+# Stateless
+# Another key feature is that REST is supposed to be stateless
+# This means one request connot depend on any other requests
+# The server only knows about the current request, and not any previous requests
+
+# For example:
+# POST item/chair creates an item
+# The server does not know the item now exists
+# GET /item/chair then goes to the database and checks to see if the item is there or an error otherwise
+# To get an item you do not need to have created an item before--the item could be in the database previously
+
+#Another example
+# A user logs in to a web application
+# The webserver does not know the user is logged in (since it does not remember any state)
+# What do we do?
+# The web application must send enough data to identify the user in every request, or else the server won't associate the request with the user
+
+# Confusing?
+# It's always confusing initially
+# As we program these APIs, a lot of these things will come naturally because often they make sense
+# Ask questions at any time, I'm always availble to help
