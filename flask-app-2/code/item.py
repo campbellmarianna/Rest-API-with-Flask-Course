e69acd31aac7a6d30bcf5d72e131980127f7f4c6
Marianna <mckd2018@gmail.com>
2019-05-20 07:52:31 -0700

    deleting our item resource from the database

--- flask-app-2/code/item.py
+++ flask-app-2/code/item.py
@@ -13,33 +13,57 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
-        # When interacting with a database the first thing you have to do is
-        # create a connection
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db') # connect to the database
+        cursor = connection.cursor() # get a cursor
+
+        query = "SELECT * FROM items WHERE name=?" # select the item
+        result = cursor.execute(query, (name,)) # run the query
+        row = result.fetchone() # fetch a row
+        connection.close() # close the connection
+
+        if row: # if the row exist its going to return a json
+            return {'item': {'name': row[0], 'price': row[1]}}
+
+    def post(self, name):
+        if self.find_by_name(name):   # Error first approach - Handle Errors then do what we want to do
+            return {"message": "An item with name '{}' already exists.".format(name)}, 400
+
+        data = Item.parser.parse_args() # parse through the data
+
+        item = {'name': name, 'price': data['price']}
+
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
         connection.close()
 
-        if row:
-            return {'item': {'name': row[0], 'price': row[1]}}
-        return {'message': 'Item not found'}, 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:   # Error first approach - Handle Errors then do what we want to do
-            return {"message": "An item with name '{}' already exists.".format(name)}, 400
-
-        data = Item.parser.parse_args()
-
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
         return item, 201
 
+    @jwt_required()
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        """
+        Delete the name in the URL from the database.
+        """
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
         return {'message': 'Item deleted'}
 
     def put(self, name):
@@ -54,7 +78,19 @@ class Item(Resource):
         return item
 
 class ItemList(Resource):
+    TABLE_NAME = 'items'
+
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+        connection.close()
+
         return {'items': items}
 
 # Left off at Retrieving our Item resources from a database 4 min in
